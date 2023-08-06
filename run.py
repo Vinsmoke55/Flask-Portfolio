85a836aff4cbc484e38d079ac5f0964e015ccbc6
VINANK <neupaneayush3@gmail.com>
2023-08-06 17:58:30 +0545

    finshed home page

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for
 
 app=Flask(__name__)
 
@@ -6,6 +6,14 @@ app=Flask(__name__)
 @app.route('/home')
 def home():
 	return render_template('home.html')
+
+@app.route('/works')
+def works():
+	return render_template('works.html')
+
+@app.route('/contact')
+def contact():
+	return render_template('contact.html')
 
 if __name__=='__main__':
 	app.run(debug=True)
